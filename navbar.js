11568dbed1b199a36b7396699c3cb8af69490f59
karina <yurch.karina@mail.ru>
2020-04-27 19:27:53 +0300

    added styles

--- navbar.js
+++ navbar.js
@@ -1,4 +1,6 @@
 window.addEventListener("scroll", function(){
+    if(window.innerWidth > 768) {
     let nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
-})
+    };
+});
