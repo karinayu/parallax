456149510e4db5c3bc892f7d27d83bb2d34c4695
karina <yurch.karina@mail.ru>
2020-04-28 21:15:19 +0300

    added mobile navbar

--- navbar.js
+++ navbar.js
@@ -1,6 +1,14 @@
 window.addEventListener("scroll", function(){
     if(window.innerWidth > 768) {
-    let nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
     };
 });
+let burger = document.querySelector(".burger");
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
+burger.onclick = () => {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+    body.classList.toggle("lock");
+}
