8795074ee3c047b9151f4ff686d48aa88f5aecbf
karina <yurch.karina@mail.ru>
2021-03-24 17:28:30 +0300

    added back-to-top button

--- navbar.js
+++ navbar.js
@@ -1,20 +1,6 @@
-/* const header = document.querySelector("header");
-if(window.innerWidth > 768) {
-    let isEvent = false;
-    window.addEventListener("scroll", function(){
-        if (!isEvent) {
-            header.classList.toggle("sticky", window.scrollY > 0);
-            console.log("вызов");
-            isEvent = true;
-            this.setTimeout(function() {
-                isEvent = false;
-            }, 500);
-        };
-    });
-}; */
 const header = document.querySelector("header");
-if(window.innerWidth > 768) {
-    window.addEventListener("scroll", function(){
+if (window.innerWidth > 768) {
+    window.addEventListener("scroll", function () {
         header.classList.toggle("sticky", window.scrollY > 0);
     });
 };
@@ -26,3 +12,30 @@ burger.onclick = () => {
     nav.classList.toggle("active");
     body.classList.toggle("lock");
 }
+(function () {
+    'use strict';
+
+    function trackScroll() {
+        var scrolled = window.pageYOffset;
+        var coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            goTopBtn.classList.add('back_to_top-show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('back_to_top-show');
+        }
+    }
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -80);
+            setTimeout(backToTop, 0);
+        }
+    }
+
+    var goTopBtn = document.querySelector('.back_to_top');
+
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+})();
